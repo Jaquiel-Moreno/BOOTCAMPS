c7212b029d5629848c631566246a88c8a8ea1fe5
unknown <jaquielmoreno@gmail.com>
2024-03-17 21:01:44 -0300

    First Commits

---
+++ product-backend/src/main/java/com/iuqaj/productbackend/ProductBackendApplication.java
@@ -0,0 +1,15 @@
+package com.iuqaj.productbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductBackendApplication.class, args);
+
+		System.out.println("ola mundo");
+	}
+
+}
